599109f7feaa0775f38dbd0f6ce4d947542fb009
Bart Thate <bthate@dds.nl>
2025-06-15 17:15:16 +0200

    add package

---
+++ nixbot/modules/tbl.py
@@ -0,0 +1,56 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "brk": "dbg",
+    "cfg": "irc",
+    "dbg": "dbg",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "hello": "hello",
+    "imp": "rss",
+    "log": "log",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "dbg": "4ab8a7924e02373e21c27cb6f66b946a",
+    "err": "fddabb7f6ea2e2fd55992fccc8738fe0",
+    "flt": "9fbbbeb19efe8f50f01831418fdc94a7",
+    "fnd": "7f257a7866ac497c3f64d0395cb3eb92",
+    "hello": "49a902bed58694a33daae4f85ca7b70f",
+    "irc": "f51789a6cd79abf8203d17e8c769b513",
+    "log": "b4b3b867ef1f40df485bc5bbf304fd0b",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "44a4fe016ad0f03c155c2401ebd3f439",
+    "slg": "99648f4d801c2356bbe18abef05e1a4b",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "15caf95bf5556e7286f7b2a64eee19ce",
+    "thr": "8ca0f9ca153fd41a4ca2dbe81e68e466",
+    "tmr": "4b8a1902295a2a465272a94a82309715",
+    "upt": "5524d8891950db0f8cbdeac67f8d5449",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
