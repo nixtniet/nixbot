f0c8cc6120c49a9c0b2112a46df44c550409353d
Bart Thate <bthate@dds.nl>
2025-09-22 15:33:14 +0200

    up version

--- nixbot/modules/tbl.py
+++ nixbot/modules/tbl.py
@@ -3,7 +3,6 @@
 
 "lookup tables"
 
-
 NAMES = {
     "cfg": "irc",
     "dis": "mdl",
@@ -36,23 +35,23 @@ NAMES = {
 }
 
 MD5 = {
-    "flt": "db9b5fb3df6d17f4221d1158796d2e9d",
-    "fnd": "93ff870591970ba7032ac7507dfb1429",
-    "irc": "6873291ccfa8eead884994aba3d3aa5c",
-    "log": "f857ae00b956a009c3ec6b9a05065450",
-    "lst": "7234a8df3c290fc41837377ff8abb912",
-    "mbx": "15c08e32a8e8c9b2860d9ac865dcb9df",
-    "mdl": "cc67c69ca157c29877c9ddcbe386beac",
+    "flt": "ea548b0df9dcb851050d5f028fa17118",
+    "fnd": "231fc7adf334c1b8a496c5fb717c1a27",
+    "irc": "09bd42f7d04f6b22b247b4d57a85bf65",
+    "log": "04daedd5761015a3fd8764de4ff1d935",
+    "lst": "b0127de287cfbdc3e4aacadc16e6b091",
+    "mbx": "f7b0aeb25ab21aee7d080c0e4181cbe9",
+    "mdl": "f1309c9b046f3fd852e28a7ca35d3525",
     "req": "793704d64443c4f19512924e294943ea",
-    "rss": "596389c2cd30686f398db619d85f0b16",
-    "rst": "30020c5f91d522a9d40b37a82795069f",
+    "rss": "a16808b8f76082ab5ffd3d02d5dfdeb3",
+    "rst": "792df24d986ad2584b6bc144be97896e",
     "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
     "tbl": "d41d8cd98f00b204e9800998ecf8427e",
-    "tdo": "e5840bc5aa1eeb2a0db415c306692b91",
-    "thr": "5cb192d8ad47295fa2f64465c6ba66fb",
-    "tmr": "5e5a0f1ebcf318fd48b6a1bd7ed98409",
-    "udp": "183bb93de61d8804fca45c2f015debba",
-    "upt": "683c8d1bf39bf5953423533086e7e710",
-    "web": "baffaaef80b19e7a0897b70d5c67ad87",
-    "wsd": "b568826c52f520dc36335092d1963c45",
+    "tdo": "e5bfce8b30b9bd6a8f2cc5138a4b0a3a",
+    "thr": "2c916d549229cec2c736e8cae51c016d",
+    "tmr": "52c99a259a1dd5b328480f6f62c8cc04",
+    "udp": "dfab7d7fe30285f8f8fc5dd1de6f1510",
+    "upt": "dea488cd5b8b1b4d2d1777fa1da487ec",
+    "web": "7858aea67b98803d5a2058d7737cfd23",
+    "wsd": "7f0b7952f3e03cd415239c87bfc8e47a",
 }
