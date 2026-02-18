9d30374a70b873ea495116a06f4c769f724767f4
Bart Thate <nixtniet@gmail.com>
2026-02-18 05:48:30 +0100

    update package

---
+++ nixbot/modules/flt.py
@@ -0,0 +1,17 @@
+# This file is placed in the Public Domain.
+
+
+from nixbot.brokers import Broker
+from nixbot.objects import Methods
+
+
+def flt(event):
+    clts = list(Broker.objs("announce"))
+    if event.args:
+        index = int(event.args[0])
+        if index < len(clts):
+            event.reply(str(clts[index]))
+        else:
+            event.reply("no matching client in fleet.")
+        return
+    event.reply(' | '.join([Methods.fqn(o).split(".")[-1] for o in clts]))
---
+++ nixbot/modules/fnd.py
@@ -0,0 +1,26 @@
+# This file is placed in the Public Domain.
+
+
+import time
+
+
+from nixbot.objects import Methods
+from nixbot.persist import Locate, Workdir
+from nixbot.utility import Time
+
+
+def fnd(event):
+    if not event.rest:
+        res = sorted([x.split('.')[-1].lower() for x in Workdir.kinds()])
+        if res:
+            event.reply(",".join(res))
+        else:
+            event.reply("no data yet.")
+        return
+    otype = event.args[0]
+    nmr = 0
+    for fnm, obj in sorted(Locate.find(otype, event.gets), key=lambda x: Time.fntime(x[0])):
+        event.reply(f"{nmr} {Methods.fmt(obj)} {Time.elapsed(time.time()-Time.fntime(fnm))}")
+        nmr += 1
+    if not nmr:
+        event.reply("no result")
