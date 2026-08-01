1eb734971bd244378d3c9137738866a679eea57f
Nixt Niet <nixtniet@gmail.com>
2026-08-02 01:25:54 +0200

    add test_commands

---
+++ testing/test_commands.py
@@ -0,0 +1,19 @@
+# This file is placed in the Public Domain.
+
+
+"commands"
+
+
+import unittest
+
+
+class TestCommands(unittest.TestCase):
+
+    def test_add(self):
+        pass
+
+    def test_command(self):
+        pass
+
+    def test_scan(self):
+        pass
