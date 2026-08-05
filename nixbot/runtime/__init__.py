e55cb60e70bcf688dc3632f411ad92356a27e33a
Nixt Niet <nixtniet@gmail.com>
2026-08-05 16:16:00 +0200

    add missing modules

---
+++ nixbot/runtime/__init__.py
@@ -0,0 +1,41 @@
+# This file is placed in the Public Domain.
+# flake8: noqa: F401
+
+
+"runtime"
+
+
+from .booting import Boot
+from .brokers import Broker, Clients
+from .clients import Buffer, Buffered, Client, Output
+from .configs import Config, Main
+from .engines import Engine
+from .package import Md5, Mods
+from .repeats import Repeater
+from .threads import Task, Thread
+
+
+
+def __dir__():
+    return (
+       'Boot',
+       'Broker',
+       'Buffer',
+       'Buffered',
+       'Client',
+       'Clients',
+       'Cmd',
+       'Config',
+       'Engine',
+       'Main',
+       'Md5',
+       'Message',
+       'Mods',
+       'Output',
+       'Repeater',
+       'Task',
+       'Thread'
+    )
+
+
+__all__ = __dir__()
